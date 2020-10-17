d40de57b698b1de57291836b400c3662e72b059c
Karenahv <837@holbertonschool.com>
2020-10-17 14:00:43 -0500

    carpeta componentes

---
+++ dragnet/src/components/AppFrame/AppFrame.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function layout(props) {
+    return (
+        <div>
+            <p>
+                Hola
+            </p>
+        </div>
+    )
+
+}
+
+export default layout
